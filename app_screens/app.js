fbadad80872bee5e37cff27899e9934fad6b67b4
David Turner <csusbdt@gmail.com>
2013-03-30 15:01:50 -0700

    refactored

--- app_screens/app.js
+++ app_screens/app.js
@@ -1,76 +1,29 @@
-window.a = { state:{}, creds:{}, menu:{} };
+window.a = { state:{}, creds:{}, menu:{}, screens:{} };
 
-(function() {
+$(function() {
  
   Screen = function(name) {
     this.name = name;
     this.$mainDiv = $('#' + name);
+    this.$menuNavElement = $('#menu-' + name);
   };
 
   a.currentScreen = 
-  a.loading       = new Screen('loading');
-  a.title         = new Screen('title');
-  a.login         = new Screen('login');
-  a.game          = new Screen('game');
+  a.screens.loading       = new Screen('loading');
+  a.screens.title         = new Screen('title');
+  a.screens.login         = new Screen('login');
+  a.screens.game          = new Screen('game');
 
   Screen.prototype.transitionTo = function(screen) {
     if (screen.onTransitionTo) screen.onTransitionTo();
     a.currentScreen.$mainDiv.fadeOut(300, function() {
+      a.currentScreen.$menuNavElement.removeClass('active');
       a.currentScreen = screen;
       a.currentScreen.$mainDiv.fadeIn(300);
+      a.currentScreen.$menuNavElement.addClass('active');
     });
   };
-
-}());
-
-a.title.initialized = false;
-
-a.title.onTransitionTo = function() {
-  if (a.title.initialized) return;
-  a.title.initialized = true;
-  var $img = $('#title-img'); 
-  $img.attr('src', 'http://graph.facebook.com/' + a.creds.uid + '/picture?type=large');
-};
-
-a.title.playNumberGame = function() {
-  a.title.transitionTo(a.game);
-};
-
-a.game.quitToTitle = function() {
-  a.game.transitionTo(a.title);
-};
-
-a.game.initialized = false;
-
-a.game.onTransitionTo = function() {
-  if (a.game.initialized) return;
-  a.game.initialized = true;
-  if (a.state.number) {
-    $('#game-num').html(a.state.number);
-  } else {
-    $.ajax({
-      url: '/op/get-num',
-      type: 'post',
-      dataType: 'json',
-      cache: false,
-      data: JSON.stringify( { 'accessToken': a.creds.accessToken } )
-    })
-    .done(function(data) {
-      if (data.login !== undefined) {
-        a.relogin(function() { initPage(); });
-      } else if (data.error !== undefined) {
-        $('#game-num').html(data.error);
-      } else {
-        a.state = data;
-        $('#game-num').html(a.state.number);
-      }
-    })
-    .fail(function(jqxhr, textStatus, errorThrown) {
-      if (errorThrown) $('#game-num').html('Error: ' + errorThrown);
-      else $('#game-num').html('Error: ' + textStatus);
-    });
-  }
-};
+});
 
 a.relogin = function(cb) {
   FB.getLoginStatus(function(response) {
@@ -79,9 +32,9 @@ a.relogin = function(cb) {
       a.creds.accessToken = response.authResponse.accessToken;
       cb();
     } else if (response.status === 'not_authorized') {
-      a.currentScreen.transitionTo(a.login);
+      a.currentScreen.transitionTo(a.screens.login);
     } else {
-      a.currentScreen.transitionTo(a.login);
+      a.currentScreen.transitionTo(a.screens.login);
     }
   }, true);
 };
@@ -93,50 +46,11 @@ a.login = function(cb) {
       a.creds.accessToken = response.authResponse.accessToken;
       cb();
     } else {
-      a.currentScreen.transitionTo(a.login);
+      a.currentScreen.transitionTo(a.screens.login);
     }
   });
 };
 
-a.menu.title = function() {
-  a.currentScreen.transitionTo(a.title);
-}
-
-a.menu.game = function() {
-  a.currentScreen.transitionTo(a.game);
-}
-
-a.menu.about = function() {
-  $("#aboutModal").modal('show');
-}
-
-a.game.incrementNumber = function() {
-  $('#game-num').html(++a.state.number);
-};
-
-a.game.saveNumber = function() {
-  $.ajax({
-    url: '/op/set-num',
-    type: 'post',
-    dataType: 'json',
-    cache: false,
-    data: JSON.stringify( { 'accessToken': a.creds.accessToken, number: a.state.number } )
-  })
-  .done(function(data) {
-    if (data.login !== undefined) {
-      a.relogin(function() { $saveBtn.click(); });
-    } else if (data.error !== undefined) {
-      $('#game-num').html(data.error);
-    } else {
-      $('#game-num').append($('<span> saved</span>'));
-    }
-  })
-  .fail(function(jqxhr, textStatus, errorThrown) {
-    if (errorThrown) $('#game-num').html('Error: ' + errorThrown);
-    else $('#game-num').html('Error: ' + textStatus);
-  });
-};
-    
 a.init = function(fbAppId) {
   FB.init({
     appId      : fbAppId,
@@ -150,10 +64,22 @@ a.init = function(fbAppId) {
     if (response.status === 'connected') {
       a.creds.uid = response.authResponse.userID;
       a.creds.accessToken = response.authResponse.accessToken;
-      a.currentScreen.transitionTo(a.title);
+      a.currentScreen.transitionTo(a.screens.title);
     } else {
-      a.currentScreen.transitionTo(a.login);
+      a.currentScreen.transitionTo(a.screens.login);
     }
   });
 };
-  
+
+a.menu.title = function() {
+  a.currentScreen.transitionTo(a.screens.title);
+}
+
+a.menu.game = function() {
+  a.currentScreen.transitionTo(a.screens.game);
+}
+
+a.menu.about = function() {
+  $("#aboutModal").modal('show');
+}
+ 
--- app_screens/fb.js
+++ app_screens/fb.js
@@ -1,18 +1,18 @@
-      (function() {
-        var n = 1;
-        window.fbAsyncInit = function() {
-          if (--n === 0) {
-            a.init('FB_APP_ID');
-          }
-        };
-      }());
+(function() {
+  var n = 2;
+  window.fbAsyncInit = function() {
+    if (--n === 0) a.init('FB_APP_ID');
+  };
+})();
 
-      (function(d) {
-        var ref = d.getElementsByTagName('script')[0];
-        var fJs = d.createElement('script');
-        fJs.id = 'facebook-jssdk'; 
-        fJs.async = true;
-        fJs.src = '//connect.facebook.net/en_US/all.js';
-        ref.parentNode.insertBefore(fJs, ref);
-      }(document));
+(function(d) {
+  var ref = d.getElementsByTagName('script')[0];
+  var fJs = d.createElement('script');
+  fJs.id = 'facebook-jssdk'; 
+  fJs.async = true;
+  fJs.src = '//connect.facebook.net/en_US/all.js';
+  ref.parentNode.insertBefore(fJs, ref);
+}(document));
       
+$(fbAsyncInit);
+
